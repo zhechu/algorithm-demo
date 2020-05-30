73bf2bda44178b7bcaf2cdd97f7869ac41a0a918
zhechu <ling-yu-wang@qq.com>
2020-05-31 00:17:14 +0800

    feature:拓扑排序

---
+++ src/main/java/com/wise/algorithm/sort/Topo.java
@@ -0,0 +1,108 @@
+package com.wise.algorithm.sort;
+
+import java.util.LinkedList;
+
+/**
+ * 拓扑排序
+ *
+ * @author lingyuwang
+ * @date 2020-05-30 23:31
+ * @since 1.0.4
+ */
+public class Topo {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(10);
+
+        // 0 -> 1 -> 3 -> 4
+        // 1 -> 4 -> 5 -> 7
+        // 2 -> 3 -> 8 -> 9
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 8);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 7);
+        graph.addEdge(8, 9);
+
+        LinkedList<Integer> list = graph.topoSortByKahn();
+        System.out.println("拓扑排序后：" + list);
+        // 拓扑排序后：[0, 2, 6, 1, 3, 4, 8, 5, 9, 7]
+    }
+
+    static class Graph {
+        /** 顶点的个数 */
+        private int v;
+        /** 邻接表 */
+        private LinkedList<Integer>[] adj;
+
+        public Graph(int v) {
+            this.v = v;
+            adj = new LinkedList[v];
+            for (int i = 0; i < v; ++i) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        /**
+         * s先于t，边s->t
+         * @param s
+         * @param t
+         */
+        public void addEdge(int s, int t) {
+            adj[s].add(t);
+        }
+
+        /**
+         * 拓扑排序
+         *
+         * 思路：
+         * 先从图中，找出一个入度为 0 的顶点，将其输出到拓扑排序的结果序列中（对应代码中就是把它打印出来），
+         * 并且把这个顶点从图中删除（也就是把这个顶点可达的顶点的入度都减 1）。循环执行以上过程，直到所有的
+         * 顶点都被输出。最后输出的序列，就是满足局部依赖关系的拓扑排序
+         */
+        public LinkedList<Integer> topoSortByKahn() {
+            LinkedList<Integer> result = new LinkedList<>();
+
+            // 统计每个顶点的入度
+            int[] inDegree = new int[v];
+            for (int i = 0; i < v; ++i) {
+                int adjSize = adj[i].size();
+                for (int j = 0; j < adjSize; ++j) {
+                    // i->w
+                    int w = adj[i].get(j);
+                    inDegree[w]++;
+                }
+            }
+
+            // 将入度为0的顶点放入队列
+            LinkedList<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < v; ++i) {
+                if (inDegree[i] == 0) {
+                    queue.add(i);
+                }
+            }
+
+            // 广度优先遍历
+            while (!queue.isEmpty()) {
+                int i = queue.remove();
+                result.add(i);
+                int adjSize = adj[i].size();
+                for (int j = 0; j < adjSize; ++j) {
+                    int k = adj[i].get(j);
+                    inDegree[k]--;
+                    if (inDegree[k] == 0) {
+                        queue.add(k);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
+
